05250e9ad6896b403e82c24791bc067d69c85ea0
alexlyan <alexlyan@yahoo.com>
2021-04-04 17:22:18 +0600

    Add solutions to sum points problem

--- Practice/sumPositiveNums.cpp
+++ Practice/sumPositiveNums.cpp
@@ -1,3 +1,4 @@
+# First Solution
 #include <vector>
 using namespace std;
 
@@ -15,4 +16,31 @@ int positive_sum (const std::vector<int> arr){
     }
   return sum_pos;
 
+}
+
+
+// Second Solution
+#include <array>
+#include <string>
+
+int points(const std::array<std::string, 10>& games) {
+  int res = 0;
+  for (const std::string& g : games)
+    res += g[0] > g[2] ? 3 : g[0] == g[2] ? 1 : 0;
+  return res;
+}
+
+//Third Solution
+
+int points(const std::array<std::string, 10>& games) {
+    int sum = 0;
+
+  for (int i = 0; i < 10; i++) {
+    int x, y;
+
+    sscanf(games[i].c_str(), "%d:%d", &x, &y);
+    sum += x == y ? 1: x > y ? 3: 0;
+
+  }
+  return sum;
 }
